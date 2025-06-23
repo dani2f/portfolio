853ec3acdf35d0445506ac92655d2728166fc2e8
Dani <daniprgm@gmail.com>
2025-06-23 11:27:22 +0200

    endpoint sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel/static';  // o serverless si prefieres
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://danielgomezfullstack.vercel.app',
-  output: 'static',
+  output: 'static',    // o 'server' si usas serverless
   adapter: vercel(),
   integrations: [
     tailwind(),
-    sitemap({
-      filter: page => !page.includes('/secret/'),
-      changefreq: 'daily',
-      priority: 0.8,
-    }),
+    sitemap(),         // aunque vayamos a exponerlo manualmente
   ],
 });
