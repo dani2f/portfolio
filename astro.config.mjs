836eaf2c535d6d1af63919f60d27d3d09b277c77
Dani <daniprgm@gmail.com>
2025-06-23 11:18:41 +0200

    cambios sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
+
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from '@astrojs/vercel/serverless';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
 	// ...
-	integrations: [tailwind()],
+	integrations: [
+		tailwind(),
+		sitemap({
+			filter: (page) => !page.includes('/secret/'),  // opcional: excluir rutas
+			changefreq: 'daily',
+			priority: 0.8,
+		  }),
+	],
 	output: 'server',
 	adapter: vercel(),
 });
